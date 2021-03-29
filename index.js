f381476810b94c88b8c5d9a9adcfacba6497b339
Isaiah Davis <ID@isaiahs-imac.myfiosgateway.com>
2021-03-29 10:13:06 -0400

    commit tree 9ea6884eaaf7ae12620267c3eacf8373bb9a9444

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers){
+    return drivers.slice(0, 2)
+} 
+const returnLastTwoDrivers = function(drivers){
+    return drivers.slice(2, 4)
+}
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers]
+ selectingDrivers.slice(0,1)
+
+ const createFareMultiplier = function (x){ 
+     return function two(y){
+        return (x * y)
+     } 
+    }
+const fareDoubler = function fares(y){
+    return (y * 2);
+}
+const fareTripler = function faresTwo(z){
+    return (z * 3);
+}
+// const selectDifferentDrivers = function (fstDrivers,scdDrivers){
+//     return scdDrivers(fstDrivers)
+        
+//     }
+function selectDifferentDrivers(fstDrivers,scdDrivers){
+    return scdDrivers(fstDrivers)
+}
+
+// function returnLastTwoDrivers(fstDrivers){
+//         return fstDrivers.slice(0,2)
+//         }
+    
+
+
